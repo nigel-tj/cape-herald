fa353bfad4779f27b79bf7afb19e68c751185cee
Nigel <hypej.10@gmail.com>
2018-02-08 23:55:11 +0200

    remove link validation

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -2,8 +2,9 @@ class Feature < ActiveRecord::Base
   #paginates_per 4 
   mount_uploader :image, ImageUploader
 
-  YT_LINK_FORMAT = /\A.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/i
-  validates :link, presence: true, format: YT_LINK_FORMAT
+  #YT_LINK_FORMAT = /\A.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/i
+  YT_LINK_FORMAT = /\A.(?:https:\/\/)?(?:www\.)?youtube\.com\/watch\?v=([a-zA-Z0-9_-]*)/
+  #validates :link, presence: true, format: YT_LINK_FORMAT
 
   mount_uploader :track, FileUploader
 
